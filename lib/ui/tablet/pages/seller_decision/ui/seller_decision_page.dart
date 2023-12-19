4615b8b9075875e0f77955daea8ba7d2c232e295
Hamza <hamzaiqa9@gmail.com>
2023-12-19 12:43:09 +0300

    fix map issue

---
+++ lib/ui/tablet/pages/seller_decision/ui/seller_decision_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SellerDecisionPage extends StatelessWidget {
+  const SellerDecisionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
